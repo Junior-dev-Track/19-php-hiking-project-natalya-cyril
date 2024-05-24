b9455fb7a8b52eea7e4f7b1e982d137f9ddfc6ad
natallya-K <kavalkova@yahoo.fr>
2024-05-25 00:11:03 +0200

    code moved from homeView to hikeList

--- src/views/detailsView.php
+++ src/views/detailsView.php
@@ -1,7 +1,6 @@
 <?php
 include 'includes/header.php';
 
-use Controllers\HikesController;
 use Models\Hikes;
 use Models\Users;
 
@@ -21,24 +20,27 @@ if ($hikeDetails === null) {
     echo "Hike not found.";
     exit;
 }
-?>
 
+    echo "<h1>Hike details</h1>";
 
-    <h1>Hike details</h1>
-<?php
     // Display the hike details
     echo "<h1>" . $hikeDetails['name'] . "</h1>";
     echo "<p>Distance: " . $hikeDetails['distance'] . " km</p>";
     // Other details
 echo "<p>Duration: " . $hikeDetails['duration'] . "</p>";
+
+// Display the hike details
 echo "<p>Elevation Gain: " . $hikeDetails['elevation_gain'] . "</p>";
 echo "<p>Description: " . $hikeDetails['description'] . "</p>";
 echo "<p>Created At: " . $hikeDetails['created_at'] . "</p>";
 echo "<p>Updated At: " . $hikeDetails['updated_at'] . "</p>";
 echo "<p>User: " . $hikeDetails['user_id'] . "</p>";
-echo "<p>User: " . $hikeDetails['user_id'] . " - " . $userDetails['username'] . "</p>";
-// Hyperlink to the home page
-echo "<a href='home'>Go to Home Page</a>";
+echo "<p>User Name: " . $userDetails['username'] . "</p>";
 
-include 'includes/footer.php'
-?>
+// Display the image from the Hikes table
+echo "<img src='" . $hikeDetails['picture_url'] . "' alt='Hike Image' width='500' height='300'>";
+
+// Hyperlink to the home page
+echo "<a href='home'>Go back</a>";
+
+include 'includes/footer.php';
--- src/views/homeView.php
+++ src/views/homeView.php
@@ -1,30 +1,12 @@
 <?php
 include 'includes/header.php';
-
-use Controllers\HikesController;
-use Models\Hikes; // Import the Hikes class
-
-$page1 = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-HikesController::test();
-HikesController::getHikeNames();
-$hikeNames = HikesController::getHikeNames($page1); // Get the first page
-$totalHikes = Hikes::getTotalHikes();
-$itemsPerPage = 6;
-$totalPages = ceil($totalHikes / $itemsPerPage);
 ?>
 
     <main>
         <h1>Home</h1>
-        <h2>List of hikes</h2>
-
-
-        <?php foreach ($hikeNames as $hike): ?>
-            <h3><a href="details?id=<?php echo $hike['id']; ?>"><?php echo $hike['name']; ?></a> - <?php echo $hike['distance']; ?> km</h3>
-        <?php endforeach; ?>
-
-        <?php for ($i = 1; $i <= $totalPages; $i++): ?>
-            <a href='?page=<?php echo $i; ?>'><?php echo $i; ?></a>
-        <?php endfor; ?>
+        <?php
+        include 'includes/hikeList.php';
+        ?>
 
     </main>
 
